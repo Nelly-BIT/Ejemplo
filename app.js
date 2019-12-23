758934a05c684f1f42d292186cc23e9d62c7e3da
NELLY <nellyo_bosqueju@hotmail.com>
2019-12-22 21:37:27 -0600

    mi primer commit

---
+++ app.js
@@ -0,0 +1,5 @@
+var frase="BOJN030796V3"
+
+var palabra= new RegExp("^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
+
+console.log(palabra.test(frase));
